1b71601eaa9e4d80c4b64eae4560febbff02515e
andrew <andry925@ukr.net>
2024-07-26 11:47:10 +0300

    feat: add functionality to hash and check hashed passwords

--- src/users/models.py
+++ src/users/models.py
@@ -1,8 +1,10 @@
 import peewee
 import peewee_async
+import bcrypt
 from decouple import config
 
 db = peewee_async.PostgresqlDatabase(database=config("DB_NAME"))
+
 
 class User(peewee.Model):
     name = peewee.CharField()
@@ -12,3 +14,14 @@ class User(peewee.Model):
     class Meta:
         database = db
         table_name = "api_user"
+
+    def set_password(self, raw_password):
+        raw_password_bytes = raw_password.encode('utf-8')
+        salt = bcrypt.gensalt()
+        self.password = bcrypt.hashpw(raw_password_bytes, salt).decode('utf-8')
+
+    def check_password(self, raw_password):
+        raw_password_bytes = raw_password.encode('utf-8')
+        return bcrypt.checkpw(
+            raw_password_bytes,
+            self.password.encode('utf-8'))
