f3001a50eabe0afdde898b1919237b1f6a8e6785
andrew <andry925@ukr.net>
2024-07-26 12:45:17 +0300

    chore: add necessary configuration for authentication

--- src/app.py
+++ src/app.py
@@ -1,8 +1,20 @@
+import aiohttp_security
+import aiohttp_session
 from aiohttp import web
+from aiohttp_security import SessionIdentityPolicy
+from aiohttp_session.cookie_storage import EncryptedCookieStorage
+from cryptography import fernet
 from .routes import setup_routes
+from src.users.views import AuthorizationPolicy
 
 
 async def create_app() -> web.Application:
     app = web.Application()
+    fernet_key = fernet.Fernet.generate_key()
+    secret_key = fernet.Fernet(fernet_key)
+    aiohttp_session.setup(app, EncryptedCookieStorage(secret_key))
+    policy = SessionIdentityPolicy()
+    auth_policy = AuthorizationPolicy()
+    aiohttp_security.setup(app, policy, auth_policy)
     setup_routes(app)
     return app
