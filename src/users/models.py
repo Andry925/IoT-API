2542e15538608078432442f6a481b46be8b5e332
andrew <andry925@ukr.net>
2024-07-26 17:25:32 +0300

    refactor: remove code related to hashing from user table

--- src/users/models.py
+++ src/users/models.py
@@ -1,27 +1,16 @@
 import peewee
 import peewee_async
-import bcrypt
 from decouple import config
+from src.utilities import PasswordHasher
 
 db = peewee_async.PostgresqlDatabase(database=config("DB_NAME"))
 
 
-class User(peewee.Model):
-    name = peewee.CharField()
+class User(peewee.Model, PasswordHasher):
+    name = peewee.CharField(max_length=128)
     email = peewee.CharField(unique=True)
     password = peewee.CharField()
 
     class Meta:
         database = db
         table_name = "api_user"
-
-    def set_password(self, raw_password):
-        raw_password_bytes = raw_password.encode('utf-8')
-        salt = bcrypt.gensalt()
-        self.password = bcrypt.hashpw(raw_password_bytes, salt).decode('utf-8')
-
-    def check_password(self, raw_password):
-        raw_password_bytes = raw_password.encode('utf-8')
-        return bcrypt.checkpw(
-            raw_password_bytes,
-            self.password.encode('utf-8'))
