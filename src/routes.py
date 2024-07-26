8208a9d4d4d1a6788d68c7d0d100df420f05bf53
andrew <andry925@ukr.net>
2024-07-26 12:46:10 +0300

    chore: add routes for authentication endpoints

--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,8 @@
-from src.users.views import test_view
+from src.users.views import test_view,create_user, login, logout
 
 
 def setup_routes(app):
     app.router.add_route('GET', '/', test_view)
+    app.router.add_route('POST', '/users', create_user)
+    app.router.add_route('POST', '/login', login)
+    app.router.add_route('POST', '/protected', logout )
