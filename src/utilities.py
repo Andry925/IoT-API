9b8c32f469df9eb7d6494bcf75a58d1402b22a8f
andrew <andry925@ukr.net>
2024-07-26 17:23:27 +0300

    refactor: move functionality related to password hashing to separate module

--- src/utilities.py
+++ src/utilities.py
@@ -1,7 +1,15 @@
-from users.models import db, User
+import bcrypt
 
-def create_tables():
-    with db:
-        db.create_tables([User])
 
-create_tables()
+class PasswordHasher:
+
+    def set_password(self, raw_password):
+        raw_password_bytes = raw_password.encode('utf-8')
+        salt = bcrypt.gensalt()
+        self.password = bcrypt.hashpw(raw_password_bytes, salt).decode('utf-8')
+
+    def check_password(self, raw_password):
+        raw_password_bytes = raw_password.encode('utf-8')
+        return bcrypt.checkpw(
+            raw_password_bytes,
+            self.password.encode('utf-8'))
