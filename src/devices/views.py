54ef5a2a03ba51241a5b82060c88f9d345c74744
andrew <andry925@ukr.net>
2024-07-27 11:38:19 +0300

    feat: implement edit/delete functionality

--- src/devices/views.py
+++ src/devices/views.py
@@ -23,7 +23,8 @@ async def create_new_device(request) -> web.Response:
         "type": device.type,
         "login": device.login,
         "location": device.location_id.name,
-        "user": device.api_user_id.email}
+        "user": device.api_user_id.email
+    }
     return web.json_response(response_obj, status=201)
 
 
@@ -36,7 +37,35 @@ async def obtain_my_devices(request) -> web.Response:
         "name": device.name,
         "type": device.type,
         "login": device.login,
-        "location": device.location_id.name,
-        "user": device.api_user_id.email
+        "location": device.location_id.name
     } for device in user_devices]
     return web.json_response(response_obj, status=200)
+
+
+async def edit_device(request) -> web.Response:
+    await check_authorized(request)
+    current_user = await authorized_userid(request)
+    device_id = int(request.match_info['device_id'])
+    data = await request.json()
+    device = await objects.get(Device, Device.id == device_id, Device.api_user_id == current_user)
+
+    requested_location = data.get("location_id")
+    if requested_location:
+        new_location_obj, created = await objects.get_or_create(Location, name=requested_location)
+        data['location_id'] = new_location_obj
+
+    for field, value in data.items():
+        setattr(device, field, value)
+
+    await objects.update(device)
+
+    return web.json_response({'success': True}, status=200)
+
+
+async def delete_device(request) -> web.Response:
+    await check_authorized(request)
+    current_user = await authorized_userid(request)
+    device_id = int(request.match_info['device_id'])
+    device = await objects.get(Device, Device.id == device_id, Device.api_user_id == current_user)
+    await objects.delete(device)
+    return web.Response(status=204)
