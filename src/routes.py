4d706ab0bf22853c638c66c6a1b9b07a8f41a5e0
andrew <andry925@ukr.net>
2024-07-27 10:16:25 +0300

    feat: add routes for create/read endpoints

--- src/routes.py
+++ src/routes.py
@@ -1,4 +1,5 @@
 from src.users.views import test_view,create_user, login, logout
+from src.devices.views import create_new_device, obtain_my_devices
 
 
 def setup_routes(app):
@@ -6,3 +7,5 @@ def setup_routes(app):
     app.router.add_route('POST', '/users', create_user)
     app.router.add_route('POST', '/login', login)
     app.router.add_route('POST', '/protected', logout )
+    app.router.add_route('POST', '/new-device', create_new_device)
+    app.router.add_route('GET', '/devices', obtain_my_devices)
