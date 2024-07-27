0c57477648e38018bc3740d2a19a457a20318ec3
andrew <andry925@ukr.net>
2024-07-27 11:38:59 +0300

    feat: add routes for edit/delete endpoints

--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,5 @@
 from src.users.views import test_view,create_user, login, logout
-from src.devices.views import create_new_device, obtain_my_devices
+from src.devices.views import create_new_device, obtain_my_devices, edit_device, delete_device
 
 
 def setup_routes(app):
@@ -9,3 +9,5 @@ def setup_routes(app):
     app.router.add_route('POST', '/protected', logout )
     app.router.add_route('POST', '/new-device', create_new_device)
     app.router.add_route('GET', '/devices', obtain_my_devices)
+    app.router.add_route('PUT', '/edit-device/{device_id}', edit_device),
+    app.router.add_route('DELETE', '/delete-device/{device_id}', delete_device)
